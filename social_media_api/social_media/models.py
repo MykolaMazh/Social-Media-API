16d24c7fe63dbc109bea27cc2ee76a25e8d716a3
MykolaMazhula <mykm3ua@gmail.com>
2025-07-27 16:02:43 +0200

    Add related names for dependant fields

--- social_media_api/social_media/models.py
+++ social_media_api/social_media/models.py
@@ -11,24 +11,30 @@ class Tag(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="posts"
+    )
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     photo = models.ImageField(upload_to="photoes/%Y/%m/%d/", blank=True, null=True)
     views = models.IntegerField(editable=False, null=True)
-    tags = models.ManyToManyField(Tag, blank=True, null=True)
-    liked = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, null=True)
+    tags = models.ManyToManyField(Tag, blank=True)
+    liked = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, blank=True, related_name="posts_liked"
+    )
 
     def __str__(self) -> str:
         return f"{self.title}/{self.author}"
 
 
 class Comment(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="comments"
+    )
     commented_date = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
 
     def __str__(self) -> str:
         return self.author
