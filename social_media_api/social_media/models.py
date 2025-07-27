584050dee8fcd65ea5f6ae710b43cb0b5e99ce71
MykolaMazhula <mykm3ua@gmail.com>
2025-07-27 15:02:28 +0200

    Create Category, Tag, Post, Comment models for social_media app

--- social_media_api/social_media/models.py
+++ social_media_api/social_media/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    photo = models.ImageField(upload_to="photoes/%Y/%m/%d/", blank=True, null=True)
+    views = models.IntegerField(editable=False, null=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, related_name="posts"
+    )
+    tags = models.ManyToManyField(Tag, blank=True, null=True)
+    liked = models.ManyToManyField(User, blank=True, null=True)
+
+    def __str__(self) -> str:
+        return f"{self.title}/{self.author}"
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    commented_date = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True)
+
+    def __str__(self) -> str:
+        return self.author
