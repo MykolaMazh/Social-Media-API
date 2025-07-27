248b2685e222f98fa212f63151153f66adaf1529
MykolaMazhula <mykm3ua@gmail.com>
2025-07-27 16:51:49 +0200

    Register social_media models in admin.py

--- social_media_api/social_media/admin.py
+++ social_media_api/social_media/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from social_media.models import Tag, Post, Comment
 
-# Register your models here.
+
+admin.site.register(Tag)
+admin.site.register(Post)
+admin.site.register(Comment)
