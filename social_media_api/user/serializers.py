e31923bc23b8c4f6ca1f114ef32a614584630c83
MykolaMazhula <mykm3ua@gmail.com>
2025-08-03 00:42:48 +0200

    Add  "posts_written", "posts_reactions","following", "followers" fields to UserSerializer

--- social_media_api/user/serializers.py
+++ social_media_api/user/serializers.py
@@ -1,8 +1,13 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from django.db.models import Count, Sum
 
 
 class UserSerializer(serializers.ModelSerializer):
+    posts_written = serializers.SerializerMethodField()
+    followers = serializers.SerializerMethodField()
+    posts_reactions = serializers.SerializerMethodField()
+
     class Meta:
         model = get_user_model()
         fields = (
@@ -12,9 +17,26 @@ class UserSerializer(serializers.ModelSerializer):
             "is_staff",
             "image",
             "about_me",
+            "posts_written",
+            "posts_reactions",
+            "following",
+            "followers",
         )
         read_only_fields = ("id", "is_staff")
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
+
+    def get_posts_written(self, obj):
+        return obj.posts.count()
+
+    def get_followers(self, obj):
+        return f"{obj.followers.count()} / {list(obj.followers.values_list('email', flat=True))}"
+
+    def get_posts_reactions(self, obj):
+        posts = obj.posts.annotate(
+            num_likes=Count("liked"), num_dislikes=Count("disliked")
+        )
+        reactions = posts.aggregate(Sum("num_likes"), Sum("num_dislikes"))
+        return f"Liked:{reactions['num_likes__sum'] or 0} / Disliked:{reactions['num_dislikes__sum'] or 0}"
 
     def create(self, validated_data):
         return get_user_model().objects.create_user(**validated_data)
