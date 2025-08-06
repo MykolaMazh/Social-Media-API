6b9df7fa3acd47ad7bfc4b616a02defed684ac33
MykolaMazhula <mykm3ua@gmail.com>
2025-08-06 19:21:39 +0200

    Add UserRetrieveSerializer, make UserSerializer inherited from it

--- social_media_api/user/serializers.py
+++ social_media_api/user/serializers.py
@@ -3,49 +3,6 @@ from rest_framework import serializers
 from django.db.models import Count, Sum
 
 
-class UserSerializer(serializers.ModelSerializer):
-    posts_written = serializers.IntegerField(read_only=True)
-    followers = serializers.SerializerMethodField()
-    posts_reactions = serializers.SerializerMethodField()
-
-    class Meta:
-        model = get_user_model()
-        fields = (
-            "id",
-            "email",
-            "password",
-            "is_staff",
-            "image",
-            "about_me",
-            "posts_written",
-            "posts_reactions",
-            "following",
-            "followers",
-        )
-        read_only_fields = ("id", "is_staff")
-        extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
-
-    def get_posts_reactions(self, obj):
-        return {"Liked": obj.total_likes, "Disliked": obj.total_dislikes}
-
-    def get_followers(self, obj):
-        return {
-            "count": obj.followers_count,
-            "emails": list(obj.followers.values_list("email", flat=True)),
-        }
-
-    def create(self, validated_data):
-        return get_user_model().objects.create_user(**validated_data)
-
-    def update(self, instance, validated_data):
-        password = validated_data.pop("password", None)
-        user = super().update(instance, validated_data)
-
-        if password:
-            user.set_password(password)
-            user.save()
-
-        return user
 
 
 class UserListSerializer(serializers.ModelSerializer):
@@ -68,3 +25,61 @@ class UserListSerializer(serializers.ModelSerializer):
 
     def get_followers(self, obj):
         return {"count": obj.followers_count}
+
+
+class UserRetrieveSerializer(UserListSerializer):
+    followers = serializers.SerializerMethodField()
+
+    class Meta:
+        model = get_user_model()
+        fields = (
+            "id",
+            "email",
+            "is_staff",
+            "image",
+            "about_me",
+            "posts_written",
+            "posts_reactions",
+            "following",
+            "followers",
+        )
+
+    def get_followers(self, obj):
+        return {
+            "count": obj.followers_count,
+            "emails": list(obj.followers.values_list("email", flat=True)),
+        }
+
+
+class UserSerializer(UserRetrieveSerializer):
+    posts_written = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = get_user_model()
+        fields = (
+            "id",
+            "email",
+            "password",
+            "is_staff",
+            "image",
+            "about_me",
+            "posts_written",
+            "posts_reactions",
+            "following",
+            "followers",
+        )
+        read_only_fields = ("id", "is_staff")
+        extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
+
+    def create(self, validated_data):
+        return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
