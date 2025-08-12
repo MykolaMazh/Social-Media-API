fbafbbd40561339a4318d6b69331a474b1de1c89
MykolaMazhula <mykm3ua@gmail.com>
2025-08-12 07:00:44 +0200

    Add swagger settings

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "social_media.apps.SocialMediaConfig",
     "debug_toolbar",
+    "drf_spectacular",
     "user.apps.UserConfig",
     "rest_framework",
     "rest_framework_simplejwt.token_blacklist",
@@ -149,6 +150,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
 SIMPLE_JWT = {
--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -21,9 +21,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from debug_toolbar.toolbar import debug_toolbar_urls
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/doc/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
     path("api/v1/user/", include("user.urls", namespace="user")),
     path(
         "api/v1/social_media/",
