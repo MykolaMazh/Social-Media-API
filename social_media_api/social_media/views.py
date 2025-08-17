66773fce51733d54b317e368b4cb9c303cfad338
MykolaMazhula <mykm3ua@gmail.com>
2025-08-17 14:04:38 +0200

    Enable getting user's and following by him users' post lists

--- social_media_api/social_media/views.py
+++ social_media_api/social_media/views.py
@@ -1,4 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from django.contrib.auth import get_user_model
 
 from social_media.models import Post
@@ -9,9 +11,25 @@ User = get_user_model()
 
 
 class PostViewSet(ModelViewSet):
-    queryset = Post.objects.select_related("author").all()
+    queryset = Post.objects.select_related("author").prefetch_related("tags")
     serializer_class = PostSerializer
     permission_classes = [IsAuthorOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+    @action(detail=False, methods=["get"])
+    def mine(self, request):
+        posts = self.get_queryset().filter(author=self.request.user)
+        serializer = self.get_serializer(posts, many=True)
+
+        return Response(serializer.data)
+
+    @action(detail=False, methods=["get"])
+    def following(self, request):
+        user = self.request.user
+        following_users = user.following.all()
+        posts = self.get_queryset().filter(author__in=following_users)
+        serializer = self.get_serializer(posts, many=True)
+
+        return Response(serializer.data)
