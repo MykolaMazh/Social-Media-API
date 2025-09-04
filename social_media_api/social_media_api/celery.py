3e92b49191d89feaa5d5ff954760af07a568f526
MykolaMazhula <mykm3ua@gmail.com>
2025-09-04 13:59:59 +0200

    Set TIME_ZONE

--- social_media_api/social_media_api/celery.py
+++ social_media_api/social_media_api/celery.py
@@ -1,5 +1,6 @@
 import os
 from celery import Celery
+from django.conf import settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "social_media_api.settings")
 
@@ -7,4 +8,8 @@ app = Celery(
     "social_media_api", broker="redis://localhost", backend="redis://localhost"
 )
 app.config_from_object("django.conf:settings", namespace="CELERY")
+
+app.conf.enable_utc = False
+app.conf.timezone = settings.TIME_ZONE
+
 app.autodiscover_tasks()
