fc86dc03d0b5d1d6b7a12d671161d2df459f2ead
MykolaMazhula <mykm3ua@gmail.com>
2025-09-04 13:57:55 +0200

    change post_publish task to handle exception

--- social_media_api/social_media/views.py
+++ social_media_api/social_media/views.py
@@ -26,25 +26,17 @@ User = get_user_model()
 
 
 class PostViewSet(ModelViewSet):
-    queryset = (
-        Post.objects.select_related("author")
-        .prefetch_related("tags", "comments__author")
-        .annotate(
-            likes=Count("liked"),
-            dislikes=Count("disliked"),
-            comments_number=Count("comments"),
-        )
-    )
     serializer_class = PostSerializer
     permission_classes = [IsAuthorOrReadOnly]
+    queryset = Post.objects.all()
 
     def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-        # if post.publish_at:
-        #     post_publish.apply_async(eta=post.publish_at, args=[post])
-        # else:
-        #     post.is_published = True
-        #     post.save()
+        post = serializer.save(author=self.request.user)
+        if post.publish_at:
+            post_publish.apply_async(eta=post.publish_at, args=[post.id])
+        else:
+            post.is_published = True
+            post.save()
 
     @action(detail=False, methods=["get"])
     def mine(self, request):
@@ -189,7 +181,16 @@ class PostViewSet(ModelViewSet):
         return Response(serializer.data)
 
     def get_queryset(self):
-        queryset = self.queryset
+        queryset = (
+            Post.objects.filter(is_published=True)
+            .select_related("author")
+            .prefetch_related("tags", "comments__author")
+            .annotate(
+                likes=Count("liked"),
+                dislikes=Count("disliked"),
+                comments_number=Count("comments"),
+            )
+        )
         tags = self.request.query_params.getlist("tag")
 
         author = self.request.query_params.get("author")
